747906a992893ed2ead0027ba5082cab4738725c
Mihail Bubnov <mihail.bubnov@mediaopt.de>
2018-02-11 16:18:33 +0300

    Final checkout

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@ double myPinsTimeShipt[] = {510.0, 320.0, 900.0, 550.0, 702.0, 601.0};
 int ledsCount = 5;
 
 unsigned long timer;
-double maxBrightnest = 255.0;
+unsigned long timerRealStart;
+unsigned long fadeInTime = 800000;
+unsigned long fadeIn = 0;
 
 void setup()
 {
+  timerRealStart = millis();
   int i;
   for (i = 0; i <= ledsCount; i++) {
     pinMode(myPins[i], OUTPUT);
@@ -17,10 +20,14 @@ void setup()
 
 void loop()
 {
-  timer = millis();
+  timer = millis() - timerRealStart;
+  fadeIn = min(fadeIn + 1, fadeInTime);
+  double fadeInMulty = ((double)fadeIn / (double)fadeInTime);
+
   int i;
+  double maxBrightnest = min(128.0, (sin((float)timer / 1500.0) * 64.0) + 128.0);
   for (i = 0; i <= ledsCount; i++) {
-    double brightnest = cos((float)timer / myPinsTimeShipt[i]) * 128 + 128;
-    analogWrite(myPins[i], (int)brightnest);
+    double brightnest = (cos((float)timer / myPinsTimeShipt[i]) * maxBrightnest) + 128.0;
+    analogWrite(myPins[i], (int)(brightnest * fadeInMulty));
   }
 }
